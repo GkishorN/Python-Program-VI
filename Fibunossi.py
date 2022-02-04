b02f2b307e0e635e728599572984430ccb919abc
GkishorN <93545399+GkishorN@users.noreply.github.com>
2022-02-04 07:45:11 +0530

    Python Program

---
+++ Fibunossi.py
@@ -0,0 +1,18 @@
+x=int(input("Enter the number of term required:"))
+s=0
+total=1
+if(x==1):
+    print(s)
+else:
+    print(s)
+    print(total)
+    for i in range(2,x):
+        tot=total+s
+        s=total
+        total=tot
+        print(tot)
+        
+    
+    
+
+    
